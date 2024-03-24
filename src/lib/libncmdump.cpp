d8a0944810d5ad51889e968889f8b7902574a8b1
TaurusXin <i@taurusxin.com>
2024-03-24 17:27:41 +0800

    feat: add dynamic library build

---
+++ src/lib/libncmdump.cpp
@@ -0,0 +1,29 @@
+#include "ncmcrypt.h"
+
+#define API __declspec(dllexport)
+
+extern "C" {
+	API NeteaseCrypt* CreateNeteaseCrypt(const char* path) {
+		return new NeteaseCrypt(std::string(path));
+	}
+
+	API int Dump(NeteaseCrypt* neteaseCrypt) {
+		try
+		{
+			neteaseCrypt->Dump();
+		}
+		catch (const std::invalid_argument& e)
+		{
+			return 1;
+		}
+		return 0;
+	}
+
+	API void FixMetadata(NeteaseCrypt* neteaseCrypt) {
+		neteaseCrypt->FixMetadata();
+	}
+
+	API void DestroyNeteaseCrypt(NeteaseCrypt* neteaseCrypt) {
+		delete neteaseCrypt;
+	}
+}
